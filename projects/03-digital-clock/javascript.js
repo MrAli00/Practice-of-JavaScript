7f38ec8c817d09d655f4f73ba54b38229ea9dbd6
Ali Hassan <130313303+MrAli00@users.noreply.github.com>
2024-12-31 20:36:54 +0000

    Digital Clock

---
+++ projects/03-digital-clock/javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const clock = document.getElementById('clock');
+  
+    setInterval(function () {
+      let date = new Date();
+      clock.innerHTML = date.toLocaleTimeString();
+    }, 1000);
+  });
+  
